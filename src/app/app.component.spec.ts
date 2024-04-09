b77b6cf2f343cc22bf9433e8315b5bec25b986d6
PavloPastushenkoo <156015193+PavloPastushenkoo@users.noreply.github.com>
2024-04-09 11:05:10 +0200

    Edizione definitiva

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'progettone'`, () => {
+  it(`should have as title 'progettoSettimanale'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('progettone');
+    expect(app.title).toEqual('progettoSettimanale');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('progettone app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('progettoSettimanale app is running!');
   });
 });
